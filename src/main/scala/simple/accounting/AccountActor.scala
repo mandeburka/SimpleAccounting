684a743ae832c2bc0da9c7f0c53fae4a392c7ea9
Artur Gavkaliuk <mandeburka@gmail.com>
2015-08-04 18:55:59 +0300

    initial

---
+++ src/main/scala/simple/accounting/AccountActor.scala
@@ -0,0 +1,46 @@
+package simple.accounting
+
+import akka.actor._
+import akka.event.Logging
+
+case class Debit(amount: Int)
+case class Credit(amount: Int)
+case object Balance
+
+class AccountActor extends Actor {
+  def receive = balanceBehaviour(0)
+  def balanceBehaviour(balance: Int): Actor.Receive = {
+    case Debit(amount) =>
+      context.become(balanceBehaviour(balance + amount))
+      sender() ! Right(balance + amount)
+    case Credit(amount) =>
+      if (balance >= amount) {
+        context.become(balanceBehaviour(balance - amount))
+        sender() ! Right(balance - amount)
+      } else {
+        sender() ! Left(s"Can not credit $amount from $balance")
+      }
+    case Balance =>
+      sender() ! Right(balance)
+  }
+}
+
+class Accounting extends Actor {
+  val log = Logging(context.system, this)
+  def receive = routerBehaviour()
+
+  def routerBehaviour(accounts: Map[Int, ActorRef] = Map.empty): Actor.Receive = {
+    case (account: Int, action: Any) =>
+      val actor = accounts.getOrElse(account, {
+        log.info(s"Creating actor for $account")
+        val accountActor = context.actorOf(Props[AccountActor])
+        context watch accountActor
+        context.become(routerBehaviour(accounts + (account -> accountActor)))
+        accountActor
+      })
+      actor.tell(action, sender())
+
+    case Terminated(a) =>
+      context.become(routerBehaviour(accounts.filterNot(_ == a)))
+  }
+}
---
+++ src/main/scala/simple/accounting/Main.scala
@@ -0,0 +1,31 @@
+package simple.accounting
+
+import akka.actor._
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.{Await}
+import scala.concurrent.duration._
+
+object Main {
+  def main (args: Array[String]) {
+    val system = ActorSystem("simpleAccounting")
+    val router = system.actorOf(Props[Accounting], "router")
+    system.actorOf(Props(classOf[Terminator], router), "terminator")
+    implicit val timeout = Timeout(25.seconds)
+
+    assert(Await.ready(router ? (1, Debit(10)), Duration.Inf).value.get.get == Right(10))
+    assert(Await.ready(router ? (1, Credit(10)), Duration.Inf).value.get.get == Right(0))
+    assert(Await.ready(router ? (1, Credit(10)), Duration.Inf).value.get.get.asInstanceOf[Either[String, Int]].isLeft)
+
+    router ! PoisonPill
+  }
+}
+
+class Terminator(ref: ActorRef) extends Actor with ActorLogging {
+  context watch ref
+  def receive = {
+    case Terminated(_) =>
+      log.info("{} has terminated, shutting down system", ref.path)
+      context.system.shutdown()
+  }
+}
